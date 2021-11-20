8daa10af9b1c601a2cf143cd5a36255dd886b9ac
mukukuu <qinii@me.com>
2021-11-20 19:47:49 +0100

    add confirm propmt for new password

--- script.js
+++ script.js
@@ -6,50 +6,55 @@ var generateBtn = document.querySelector("#generate");
 
 //to generate 8 - 128 long password
 document.getElementById("generate").addEventListener("click", writePassword());
-   //ask if user want a password between 8 - 128
-   var promptlength = window.prompt('Would you like to generate a secure password with more than 8 characters, includes uppercase letters, lower case letters and special characters? Enter "yes" only to generate.');
-     if (promptlength === "yes" || promptlength === "YES") {
-     }
-     if (promptlength) {
-      writePassword()
-      
-     };
-//to generate to to include lowercase, uppercase, numeric, and/or special characters
-document.getElementById("generate").addEventListener("click", writePassword());
-   //ask user
-   var promptlength = window.prompt('Would you like to generate a secure password to include lowercase, uppercase, numeric, and/or special characters? Enter "yes" only to generate.');
-     if (promptlength === "yes" || promptlength === "YES") {
-     }
-     if (promptlength) {
-      writePasswordS()
-     };
+//ask if user want a password between 8 - 128
+var promptlength = window.prompt('Would you like to generate a secure password with more than 8 characters, includes uppercase letters, lower case letters and special characters? Enter "8" or "128"  to generate 8 - 128 long random password.');
+if (promptlength === "8" || promptlength === "128") {
+}
+if (promptlength) {
+  writePassword()
+
+  var confirmchara = window.confirm("Would you like to include lowercase, uppercase, numeric and special characters?"); 
+console.log(confirmchara);
+  if (confirmchara) {
+    window.alert("generating new password...");
+    if (confirmchara === "yes") {
+      var confirmchara = window.confirm("Return to prvious window for a secure password.");
+    }
+
+  }
+
+};
+
 
 // Write password to the #password input
-function writePassword() { 
+function writePassword() {
   //generate 10 characters password
-{ var chars = "1234567890"
-  var passwordLength = 100;
-  var password = "";
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber,randomNumber + 1);
-} 
-  document.getElementById("password").value = password;
-  
-}
+  {
+    var chars = "1234567890";
+    var passwordLength = 100;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById("password").value = password;
+
+  }
 };
-//generate 
-function writePasswordS() { 
+
+function writePassword() {
   //generate 10 characters password
-{ var chars = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()"
-  var passwordLength = 100;
-  var password = "";
-for (var i = 0; i <= passwordLength; i++) {
-  var randomNumber = Math.floor(Math.random() * chars.length);
-  password += chars.substring(randomNumber,randomNumber + 1);
-} 
-  document.getElementById("password").value = password;
-}
+  {
+    var chars = "1234567890qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()_+";
+    var passwordLength = 100;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * chars.length);
+      password += chars.substring(randomNumber, randomNumber + 1);
+    }
+    document.getElementById("password").value = password;
+
+  }
 };
 
 
